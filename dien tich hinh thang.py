5ad36bc03cb74d961d6f7cc079d25872c2692738
dungba67khmt-netizen <dung.ba.67khmt@ntu.edu.vn>
2025-11-18 20:53:14 +0700

    dien tich hinh thang

---
+++ dien tich hinh thang.py
@@ -0,0 +1,5 @@
+đay_lon = float(input("Nhập độ dài đáy lớn: "))
+đay_be = float(input("Nhập độ dài đáy bé: "))
+chieu_cao = float(input("Nhập chiều cao: "))
+dien_tich = (đay_lon + đay_be) * chieu_cao / 2
+print("Diện tích hình thang là:", dien_tich)
