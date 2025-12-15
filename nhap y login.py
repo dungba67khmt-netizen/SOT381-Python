39b4d384169ae601fe5e0bea6a93ee18bfa904f2
dungba67khmt-netizen <dung.ba.67khmt@ntu.edu.vn>
2025-12-15 14:53:10 +0700

    Create nhap y login.py

---
+++ nhap y login.py
@@ -0,0 +1,8 @@
+while True:
+    user_input=input('nhap ten ng dung:')
+    if user_input=='y':
+        print('login sucess')
+        break
+    else:
+        print('vui long nhap lai')
+        
