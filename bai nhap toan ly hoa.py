fa85bb00439f757d4de1b0d5e8862eaaedbcb008
dungba67khmt-netizen <dung.ba.67khmt@ntu.edu.vn>
2025-12-29 14:41:36 +0700

    Create bai nhap toan ly hoa.py

---
+++ bai nhap toan ly hoa.py
@@ -0,0 +1,17 @@
+a=int(input('nhap diem toan:'))
+b=int(input('nhap diem ly:'))
+c=int(input('nhap diem hoa:'))
+Tong=a+b+c
+if Tong >=15 and a>=4 and b>=4 and c>=4:
+      print('dau')
+      if a>=5 and b>=5 and c>=5:
+          print('hc deu tat ca cac mon')
+      else:
+        print('hc chua deu')
+else:
+        print('thi hong')
+
+    
+
+      
+      
