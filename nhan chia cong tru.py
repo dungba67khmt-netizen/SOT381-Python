97d7cafc599b3e1f642e5d32c2d1631a19163e51
dungba67khmt-netizen <dung.ba.67khmt@ntu.edu.vn>
2025-12-29 14:52:02 +0700

    Create nhan chia cong tru.py

---
+++ nhan chia cong tru.py
@@ -0,0 +1,12 @@
+a = int(input("Nhập số nguyên a: "))
+b = int(input("Nhập số nguyên b: "))
+
+print("a + b =", a + b)
+print("a - b =", a - b)
+print("a * b =", a * b)
+
+if b != 0:
+    print("a / b =", a / b)
+    print("a // b =", a // b)
+else:
+    print("0 chia dc cho 0")
